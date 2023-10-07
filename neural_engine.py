8cf660a4b66bcaef4333e8aa1177f70f4e696c54
Turfader <jbouyer151@gmail.com>
2023-10-07 12:59:48 -0700

    added parameters

--- neural_engine.py
+++ neural_engine.py
@@ -7,6 +7,14 @@ from utils import *
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Hyper-parameters
+input_size: int  # TODO define this later
+hidden_size: int  # TODO define this later
+num_classes: int  # TODO define this later
+num_epochs: int  # TODO define this later
+batch_size: int  # TODO define this later
+learning_rate: int = 0.001  # I have no clue what this does, but this is what the tutorial had. Change later?
+
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())  # double check the file source
 # I need to review the documentation for this, but it should properly import most of the dependencies that we need
 # we need to come back to this later, but good enough for now
