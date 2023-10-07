303e504dfc69f977a38313b694a289431b0d9059
Turfader <jbouyer151@gmail.com>
2023-10-07 09:56:22 -0700

    added code not written error

--- neural_engine.py
+++ neural_engine.py
@@ -3,8 +3,11 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from main import CodeNotWrittenError
 
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())  # double check the file source
 # I need to review the documentation for this, but it should properly import most of the dependencies that we need
 # we need to come back to this later, but good enough for now
 dataset = DataLoader(train, 32)
+
+raise CodeNotWrittenError
