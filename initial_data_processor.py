114d958aa8560ec0d5223269ea88c58b0f7973d1
John Bouyer <jbouyer151@gmail.com>
2023-10-07 09:12:00 -0700

    Added nasa provided code for data visualization

---
+++ initial_data_processor.py
@@ -0,0 +1,5 @@
+# code provided from nasa
+
+import pandas
+data = pandas.read_csv("dsc_fc_summed_spectra_2016_v01.csv", delimiter = ',', parse_dates=[0],
+                       infer_datetime_format=True, na_values='0', header = None)
