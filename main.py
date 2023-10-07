d14a038d207b21c76e697b0fc85647aca59a265d
Turfader <jbouyer151@gmail.com>
2023-10-07 10:02:59 -0700

    added package imports and venv setup. Add more package imports here

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import sys
+import venv
+from os import getcwd, path
+from subprocess import run
+import time as time
+
+venv_folder = path.join(getcwd(), "subprocess_venv")
+if not path.exists(venv_folder):
+
+    start = time()
+
+    print("Creating virtual environment")
+    venv.create("subprocess_venv", with_pip=True)
+
+    print("Installing dependencies...")
+
+    # Add pip installation names here for any new package.
+    packages = ["numpy", "torch"]
+    for package in packages:
+        run([sys.executable, "-m", "pip", "install", package], check=True)
+        print(f"Installed package: {package}")
+
+    print(f"Venv creation completed in {round(time()-start, 2)}s")
+
+
 class CodeNotWrittenError(Exception):
     def __init__(self, message="This code is not implemented yet"):
         self.message = message
