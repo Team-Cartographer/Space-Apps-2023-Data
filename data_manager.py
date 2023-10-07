89895b80710c51f7d33a8562269e206d5c671420
abhi arya <abhia9167@gmail.com>
2023-10-07 10:47:05 -0700

    C:/Users/ashwa/AppData/Local/Programs/Python/Python311/python.exe

--- data_manager.py
+++ data_manager.py
@@ -7,23 +7,41 @@ from utils import *
 from datetime import datetime
 from vec3 import *
 
-year = 2019
+year, data = 2019, []
 data_folder_path: str = "C://Users//ashwa//Desktop//DSCOVR_Data"
-data_file_path: str = data_folder_path + f"//dsc_fc_summed_spectra_{year}_v01.csv"
+data_file_path: str = data_folder_path + f"//dsc_fc_summed_spectra_{year}_vrow1.csv"
 
 # access the data (for now, change this to your local data location) 
-
 with open(data_file_path, mode="r") as data_file:
     data: list = list(csv.reader(data_file, delimiter=','))
     data_file.close()
 
-datetime: str = data[0][0]
-mag_field: Vec3 = Vec3(float(data[0][1]), float(data[0][2]), float(data[0][3]))
-flux_measurements: list = [float(data[0][x]) for x in range(4, 53)]
+def get_datetime(row: list) -> str: 
+    datetime: str = data[0][0]
 
-data_object = [datetime, mag_field, flux_measurements]
+def get_mag_field_vec(row: list) -> Vec3:
+    mag_field: Vec3 = Vec3(float(data[row][1]), float(data[row][2]), float(data[row][3]))
+    return mag_field
+
+def get_flux_measurements(row: list) -> list: 
+    flux_measurements: list = [float(data[row][x]) for x in range(4, 53)]
+    for i in range(len(flux_measurements)):
+        if flux_measurements[i] == 0:
+            flux_measurements[i] == "NaN"
+        
+    return flux_measurements
+
+def get_data_object(row: list, display_flux: bool) -> list:
+    data_object = []
+    if display_flux:
+        data_object = [get_datetime(row), get_mag_field_vec(row), get_flux_measurements(row)] 
+    else:
+        data_object = [get_datetime(row), get_mag_field_vec(row)]
+    
+    return data_object
 
 if __name__ == "__main__":
-    print(mag_field)
+    for row in len(data):
+        print(get_data_object(row, display_flux=False))
 
 
