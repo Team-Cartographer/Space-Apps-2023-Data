949d873e7fd2c00794c2a64a3b4de769f1872f29
abhi arya <abhia9167@gmail.com>
2023-10-07 09:37:13 -0700

    added templates

---
+++ backup_engine.py
@@ -0,0 +1,11 @@
+class KalmanFilter:
+    def __init__(self):
+        pass
+
+class LowPassFilter: 
+    def __init__(self):
+        pass
+
+class MovingAvgFilter:
+    def __init__(self):
+        pass
---
+++ data_manager.py
@@ -0,0 +1,2 @@
+
+# get the data stream 
