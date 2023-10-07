678451bae1806b276b354dc5e3446ca4e25558c0
abhi arya <abhia9167@gmail.com>
2023-10-07 11:00:15 -0700

    data manager complete

--- data_manager.py
+++ data_manager.py
@@ -5,25 +5,26 @@ import csv
 
 from utils import * 
 from datetime import datetime
-from vec3 import *
+from tqdm import tqdm 
 
-year, data = 2019, []
+year = 2019
 data_folder_path: str = "C://Users//ashwa//Desktop//DSCOVR_Data"
-data_file_path: str = data_folder_path + f"//dsc_fc_summed_spectra_{year}_vrow1.csv"
+data_file_path: str = data_folder_path + f"//dsc_fc_summed_spectra_{year}_v01.csv"
 
 # access the data (for now, change this to your local data location) 
 with open(data_file_path, mode="r") as data_file:
     data: list = list(csv.reader(data_file, delimiter=','))
     data_file.close()
 
-def get_datetime(row: list) -> str: 
-    datetime: str = data[0][0]
+def get_datetime(row: int) -> str: 
+    datetime: str = data[row][0]
+    return datetime
 
-def get_mag_field_vec(row: list) -> Vec3:
-    mag_field: Vec3 = Vec3(float(data[row][1]), float(data[row][2]), float(data[row][3]))
+def get_mag_field_vec(row: int):
+    mag_field = (float(data[row][1]), float(data[row][2]), float(data[row][3]))
     return mag_field
 
-def get_flux_measurements(row: list) -> list: 
+def get_flux_measurements(row: int) -> list: 
     flux_measurements: list = [float(data[row][x]) for x in range(4, 53)]
     for i in range(len(flux_measurements)):
         if flux_measurements[i] == 0:
@@ -31,7 +32,7 @@ def get_flux_measurements(row: list) -> list:
         
     return flux_measurements
 
-def get_data_object(row: list, display_flux: bool) -> list:
+def get_data_object(row: int, display_flux: bool) -> list:
     data_object = []
     if display_flux:
         data_object = [get_datetime(row), get_mag_field_vec(row), get_flux_measurements(row)] 
@@ -41,7 +42,14 @@ def get_data_object(row: list, display_flux: bool) -> list:
     return data_object
 
 if __name__ == "__main__":
-    for row in len(data):
-        print(get_data_object(row, display_flux=False))
+    data_list = []
+    for i in tqdm(range(len(data)), desc="Compiling Data"): 
+        data_list.append(get_data_object(i, display_flux=False))
+    
+    # not working, but leave it for now 
+    # x = input(f"Enter a date in '{year}-MM-DD' format: ")
+    # for daily_data in data_list:
+    #     if(x == daily_data[0].split()[0]):
+    #         print(daily_data)
 
 
--- initial_data_processor.py
+++
@@ -1,6 +0,0 @@
-from utils import *
-# code provided from nasa
-
-import pandas
-data = pandas.read_csv("dsc_fc_summed_spectra_2016_v01.csv", delimiter = ',', parse_dates=[0],
-                       infer_datetime_format=True, na_values='0', header = None)
