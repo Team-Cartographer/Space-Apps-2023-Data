8d8c155c55c646a16b8eecd3bbba6ffd81b6ca7e
abhi arya <abhia9167@gmail.com>
2023-10-07 09:54:45 -0700

    added boilerplate for data_manager

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,13 @@
+import numpy as np
+import os  
+import math 
+import csv 
 
-# get the data stream 
+
+# access the data (for now, change this to your local data location) 
+year = 2016
+data_folder_path: str = "C://Users//ashwa//Desktop//DSCOVR_Data"
+data_file_path: str = data_folder_path + f"//dsc_fc_summed_spectra_{year}_v01.csv"
+
+with open(data_file_path, mode="r") as f:
+    pass
