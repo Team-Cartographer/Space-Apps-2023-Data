7a5e05b45bfaf46fda9270a0ab371c31c99a355b
areich128 <alex.reich128@gmail.com>
2023-10-08 11:48:31 -0600

    added algo to determine QDC

--- oracle_engine.py
+++ oracle_engine.py
@@ -24,7 +24,7 @@ def quiet_day(date, time, kp_list):
     # dictframe['day'] = pd.to_datetime(dictframe['day'])
     filtered_days = dictframe.groupby('day')['kp'].apply(lambda x: (x > threshold).any())
     quiet_day_list = filtered_days[filtered_days == False].index.tolist()
-    print(quiet_day_list)
+    # print(quiet_day_list)
 
     # getting the average kp value for those days
     data = {
@@ -39,18 +39,28 @@ def quiet_day(date, time, kp_list):
     # print(filtered_df)
     avg_kp = filtered_df.groupby('date')['Kp'].mean()
 
-    #now normalizing the data and averaging to finally get QDC
+    # overall averge mean of kp value on quiet days
+    ovr_avg_kp = avg_kp.mean()
+    sum_quotients = 0
+    # now normalizing the data and averaging to finally get QDC
+    for i in kp_list:
+        quotient = int(i)/ovr_avg_kp
+        sum_quotients += quotient
 
-    return avg_kp
+    qdc = sum_quotients/len(kp_list)
+
+    # print(ovr_avg_kp)
+
+    return qdc
 
 
 tot_qdc = quiet_day(date_list, time_list, kp_val)
 print(tot_qdc)
 
 
-def find_kp(x, y, qdc, _C):
+def find_kp(x, y, qdc, _c):
     h_val = np.sqrt((x ** 2)+(y ** 2))
     h_max = np.max(h_val)
     a = (h_max - qdc)/qdc
-    k_p = 0.67 * np.log10(a) + _C
+    k_p = 0.67 * np.log10(a) + _c
     return k_p
