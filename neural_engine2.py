b5f1d7def81899b3dc01579824a85a8f068f83b7
Turfader <jbouyer151@gmail.com>
2023-10-08 13:08:10 -0700

    adds pickling and writing to csv. This should work

--- neural_engine2.py
+++ neural_engine2.py
@@ -2,7 +2,9 @@ import numpy as np
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 import data_manager as dm 
-import csv 
+import csv
+import pickle
+
 
 def train_xgb_model(X, kp, num_round=500, early_stopping_rounds=None):
     # Split the data into training and validation sets
@@ -29,27 +31,49 @@ def train_xgb_model(X, kp, num_round=500, early_stopping_rounds=None):
 
     return xgb_model
 
+
 def predict_kp(xgb_model, X_test):
     dtest = xgb.DMatrix(X_test)
     kp_predictions = xgb_model.predict(dtest)
     return kp_predictions
 
+def train_xgb():
+    xgb_model = train_xgb_model(X, Y, num_round=100,
+                                early_stopping_rounds=10)  # TODO JL setup a pickle file for this thing
+
+    # Pickles xgb_model
+    with open('xgb_model.pickle', 'wb') as model_file:
+        pickle.dump(xgb_model, model_file)
+
+    return xgb_model
+
+
 if __name__ == "__main__":
     dataset = dm.get_training_data()[0]
-    X = np.array([arr[2] for arr in dataset]).reshape(-1, 1) # average filtered data (get past reshape issues) 
-    Y = np.array([arr[0] for arr in dataset]) # kp values
+    X = np.array([arr[2] for arr in dataset]).reshape(-1, 1)  # average filtered data (get past reshape issues)
+    Y = np.array([arr[0] for arr in dataset])  # kp values
     print(X.shape, Y.shape)
 
     # Train the XGBoost model
-    xgb_model = train_xgb_model(X, Y, num_round=100, early_stopping_rounds=10) # TODO JL setup a pickle file for this thing 
+    train_xgb()
+
     print("+------+ Training Complete! +------+")
 
     # Predict kp values using the trained model
     new_data = np.array([arr[1] for arr in dataset]).reshape(-1, 1)
-    kp_predictions = predict_kp(xgb_model, new_data) # TODO check for xgb model via pickle before creating a new one 
+    try:
+        with open('xgb_model.pickle', 'rb') as model_file:
+            loaded_xgb_model = pickle.load(model_file)
+            kp_predictions = predict_kp(loaded_xgb_model, new_data)
+    except e:
+        kp_predictions = predict_kp(train_xgb_model(), new_data)  # TODO check for xgb model via pickle before creating a new one
 
     print("Predicted Kp Values: ", kp_predictions)
 
-    #TODO write kp_predictions to a CSV
-    #with open("predictions.csv", "w") as f:
-    #    writer = csv.writer(f, delimiter=',')
+    # TODO write kp_predictions to a CSV
+    with open("predictions.csv", "w", newline="\n") as file:
+        writer = csv.writer(file, delimiter=',')
+        for row in kp_predictions:
+            csv_writer.writerow(row)
+
+    print(f'Data from kp_predictions has been written to {csv_file_name}.')
