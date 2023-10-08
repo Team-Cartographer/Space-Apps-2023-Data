e94d3290a380fcbb3a1e8c4aafd9bf38b2714da9
abhi arya <abhia9167@gmail.com>
2023-10-08 16:08:34 -0700

    reordered files

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-import sys
-import venv
-from os import getcwd, path
-from subprocess import run
-from utils import *
-
-venv_folder = path.join(getcwd(), "subprocess_venv")
-if not path.exists(venv_folder):
-
-    print("Creating virtual environment")
-    venv.create("subprocess_venv", with_pip=True)
-
-    print("Installing dependencies...")
-
-    # Add pip installation names here for any new package.
-    packages = ["numpy", "torch", "torchvision", "tensorflow"]
-    for package in packages:
-        run([sys.executable, "-m", "pip", "install", package], check=True)
-        print(f"Installed package: {package}")
-
-    print(f"Venv creation completed")
+from utils import * 
 
 
-if __name__ == "__main__":
-    raise CodeNotWrittenError
-
+# write integration of data manager, neural engine, and oracle engine
+raise CodeNotWrittenError
   
