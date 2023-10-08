3a815fca2eec0e409f6a8b00c28515177455db95
areich128 <alex.reich128@gmail.com>
2023-10-08 10:49:16 -0600

    added algo to determine avg Kp on quiet days

--- oracle_engine.py
+++ oracle_engine.py
@@ -8,9 +8,9 @@ from test_bench import kp_val
 from test_bench import di
 
 
-print(len(date_list))
-print(len(time_list))
-print(len(kp_val))
+# print(len(date_list))
+# print(len(time_list))
+# print(len(kp_val))
 
 
 def quiet_day(date, time, kp_list):
@@ -18,21 +18,30 @@ def quiet_day(date, time, kp_list):
     itms = 0
     avg = 0
     threshold = 2
-    for i in kp_list:
-        if i <= 2:
-            sum += i
-            itms += 1
-            # print(date[i])
-    k_avg = sum/itms
 
+    # getting list of quiet days
     dictframe = pd.DataFrame({'day': date, 'time': time, 'kp': kp_list})
-    dictframe['day'] = pd.to_datetime(dictframe['day'])
+    # dictframe['day'] = pd.to_datetime(dictframe['day'])
     filtered_days = dictframe.groupby('day')['kp'].apply(lambda x: (x > threshold).any())
     quiet_day_list = filtered_days[filtered_days == False].index.tolist()
+    print(quiet_day_list)
 
-    # print(quiet_day_list)
+    # getting the average kp value for those days
+    data = {
+        'date': date_list,
+        'time': time_list,
+        'Kp': kp_list
+    }
+    df = pd.DataFrame(data)
+    # print(data)
+    datetime_list = (df['date'])
+    filtered_df = df[df['date'].isin(quiet_day_list)]
+    # print(filtered_df)
+    avg_kp = filtered_df.groupby('date')['Kp'].mean()
 
-    return quiet_day_list
+    #now normalizing the data and averaging to finally get QDC
+
+    return avg_kp
 
 
 tot_qdc = quiet_day(date_list, time_list, kp_val)
