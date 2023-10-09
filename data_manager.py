94c0f3ee7020db1b64a4f93e96e4581e19b27aba
abhi arya <abhia9167@gmail.com>
2023-10-08 18:35:02 -0700

    neural engine complete

--- data_manager.py
+++ data_manager.py
@@ -27,40 +27,35 @@ def setup_data(data) -> np.ndarray:
 
 
 @timeit
-def create_dataset(data, kpData):
+def create_dataset(data):
     first_reading = data['mag_field'][0]
     filt = KalmanFilter3D([first_reading[0], first_reading[1], first_reading[2]])
     filtered_data = filt.filter_measurements(data['mag_field'])
 
     dataset = []
     temp_row = []
-    filtered_avg = []
 
     for i in tqdm(range(len(data)), desc="Building Dataset"): 
         if i % 180 == 0: 
             # 3 hour gap every 180 points  
             #print(f"[{temp_row[0]}, {temp_row[1][0:10]}]\n") if i != 0 else None
-            if i != 0:
-                average = np.mean(np.mean(filtered_avg, axis=1))
-                temp_row.append(average)
             dataset.append(temp_row) # if i != 0 else None 
             temp_row = []
 
             start_time = data['datetime'][i]
             start_date = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
             #end_time = (start_date + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S")
-            kp_value = int(kpData[start_date.strftime("%Y-%m-%d %H:%M:%S")])
+            #kp_value = int(kpData[start_date.strftime("%Y-%m-%d %H:%M:%S")])
 
             #temp_row.append(start_time)
             #temp_row.append(end_time)
             #temp_row.append([]) # raw mag field
             #temp_row.append([]) # faraday data
 
-            temp_row.append(kp_value)
+            #temp_row.append(kp_value)
             temp_row.append([]) # filtered mag field 
         else: 
-            filtered_avg.append(filtered_data[i])
-            temp_row[1].append(filtered_data[i])
+            temp_row[0].append(filtered_data[i])
             #temp_row[2].append(data['mag_field'][i])
             #temp_row[3].append(data['flux_measurements'][i])
 
@@ -68,21 +63,18 @@ def create_dataset(data, kpData):
 
 
 def get_dataset(start_year=2016, years=1, print_year=False):
-    if years > 5 or start_year > 2021:
-        raise ValueError # years cannot be greater than 5 until alex finishes kp data calculation 
-    
     rows, all_datasets = [], []
-    KpDict: dict = {}
-    with open('data/historic_kp_data.dat', mode='r') as f:
-        for line in f:
-            fields = line.strip().split()
-            rows.append(fields)
-            frmt = "%Y-%m-%d %H:%M:%S"
-            timestr = fields[0] + " " + fields[1]
-            timestamp = datetime.strptime(timestr[0:len(timestr) - 4], frmt)
-            KpDict.update({str(timestamp): int(fields[3][0:1])})
+    # KpDict: dict = {}
+    # with open('data/historic_kp_data.dat', mode='r') as f:
+    #     for line in f:
+    #         fields = line.strip().split()
+    #         rows.append(fields)
+    #         frmt = "%Y-%m-%d %H:%M:%S"
+    #         timestr = fields[0] + " " + fields[1]
+    #         timestamp = datetime.strptime(timestr[0:len(timestr) - 4], frmt)
+    #         KpDict.update({str(timestamp): int(fields[3][0:1])})
         
-        f.close()
+    #     f.close()
     
 
     for i in range(0, years): 
@@ -102,7 +94,7 @@ def get_dataset(start_year=2016, years=1, print_year=False):
                 data_file.close()
 
             processed_data = setup_data(data)
-            cleaned_data = create_dataset(processed_data, KpDict)
+            cleaned_data = create_dataset(processed_data)
 
             with open(fpth, 'wb') as f:
                 pickle.dump(cleaned_data, f)
--- neural_engine3.py
+++ neural_engine3.py
@@ -4,39 +4,53 @@ import numpy as np
 import data_manager as dm 
 import matplotlib.pyplot as plt
 
-# get data
-dataset = dm.get_dataset(start_year=2017)[0]
-X = np.array([arr[1][0] for arr in dataset]) # average filtered data (get past reshape issues)
-print(X)
-Y = np.array([arr[0] for arr in dataset])  # kp values
+# get training dataset --------
+# dataset = dm.get_dataset(start_year=2017)[0] 
+# X = np.array([arr[1][0] for arr in dataset]) # average filtered data (get past reshape issues)
+# #print(X)
+#Y = np.array([arr[0] for arr in dataset])  # kp values
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-print(X_train.shape)
+# The TensorFlow Model: (Uncomment to process this) 
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+# #print(X_train.shape)
 
-# set up tensorflow model 
-model = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(3, 1)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(1,)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(1)  # Output layer for regression task
-])
+# # set up tensorflow model --------
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Input(shape=(3, 1)),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation='relu', input_shape=(1,)),
+#     tf.keras.layers.Dense(64, activation='relu'),
+#     tf.keras.layers.Dense(32, activation='relu'),
+#     tf.keras.layers.Dense(1)  # Output layer for regression task
+# ])
 
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-model.fit(X_train, Y_train, epochs=250, batch_size=32, validation_split=0.2)
+# model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+# model.fit(X_train, Y_train, epochs=250, batch_size=32, validation_split=0.2)
 
-loss, mae = model.evaluate(X_test, Y_test)
+# for testing purposes 
+#loss, mae = model.evaluate(X_test, Y_test)
 #print("Mean Absolute Error:", mae)
 
-data2 = dm.get_dataset(start_year=2016)[0]
-new_X = np.array([arr[1][0] for arr in data2])
-predictions = model.predict(new_X)
-predictions = [np.round(prediction) for prediction in predictions]
+# model.save('data/models/predictor') # UNCOMMENT ALL ABOVE CODE TO GET THE MODEL 
 
-plt.scatter(np.arange(len(Y)), Y, label='Original Data', color='blue')
-plt.scatter(np.arange(len(predictions)), predictions, label='Predictions', color='red')
-plt.xlabel('X Values')
-plt.ylabel('Y Values')
-plt.legend()
-plt.show()
+#print(len(predictions))
+# visualize (testing)
+# plt.scatter(np.arange(len(predictions)), predictions, color='blue')
+# plt.xlabel('X Values')
+# plt.ylabel('Y Values')
+# plt.legend()
+# plt.show()
+
+def get_neural_Kp_data(dX, YEAR):
+    model = tf.keras.models.load_model('data//models//predictor')
+    new_X = dX
+    predictions = model.predict(new_X)
+    predictions = [np.round(prediction) for prediction in predictions]
+    with open(f'data/preds/{YEAR}_predictions.csv', "w", newline="\n") as f:
+        for prediction in predictions:
+            f.write(f"{int(prediction[0])},")
+
+if __name__ == "__main__":
+    data2 = dm.get_dataset(start_year=2023)[0]
+    x = np.array([arr[0][0] for arr in data2])
+    get_neural_Kp_data(x, 2023)
